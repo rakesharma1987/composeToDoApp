b5cb0d44884098b883a1ab131d3ef2ed78fc7a01
Rakesh <rakesh@avsstech.com>
2024-10-25 11:38:36 +0530

    Initial commit

---
+++ app/src/main/java/com/sharmaapp/mytodoapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.sharmaapp.mytodoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sharmaapp.mytodoapp.ui.theme.MyTodoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyTodoAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainPage(){
+    Column {
+
+    }
+}
